86a8b08e57b34c2140a0608009111c877bc4883d
Kamiikrazy <bobmcbob717@gmail.com>
2024-11-12 13:16:35 +0000

    I forgor something

--- main.c
+++ main.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "./src/kvector.h"
+#include <string.h>
+#include "src/defs.h"
 
 int main() {
-    kvector kv;
-    kvector_init(&kv);
-
-    kvector_add(&kv, "Wassup gangalang");
-    for(i=0; i<kvector_total(&kv); i++) {
-        printf()
-    }
+    
 }
