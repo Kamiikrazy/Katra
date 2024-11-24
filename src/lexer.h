c962f5a13f1a682c4f750a8a5742f7ff0ea176d3
Kamiikrazy <bobmcbob717@gmail.com>
2024-11-24 16:16:16 +0000

    Added punctuation parsing + fixed lexer confusing operators :D

--- src/lexer.h
+++ src/lexer.h
@@ -3,14 +3,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-char src[] = "a + b = 2 if a == 3";
+char src[] = "public function main(a: int, b: int) { return a + b; }";
 int line;
 
 typedef enum {
     Num, Fn, Pub, Loc, If, Else, Orif, Return, Try, While, For, Pass, Break, Do,
-    String, Int, Float, Bool, Or, And, Not, Eq, Ne, Eqeq, Lt, Gt, Le, Ge,
+    String, Int, Float, Bool, Null, True, False, Module, Or, And, Not, Eq, Ne, Eqeq, Lt, Gt, Le, Ge,
     Add, Sub, Mul, Div, Mod, Inc, Dec, Muleq, Diveq, 
-    Dot, Comma,
+    Dot, Comma, Semcol, Col, Dbcol, Bsl, Dbbsl, Dbfsl, Hash, Opb, Clb, Opclb, Clclb,
     Huh,
 } Token;
 
@@ -21,7 +21,11 @@ const char *keywords[] = {
 
 const char *operators[] = {
     "+", "-", "/", "*", "=", "=/=", "==", "+=", "-=", "/=", "*=", "<", 
-    ">", "<=", ">=", "%",
+    ">", "<=", ">=", "%", "&", "||", "!"
+};
+
+const char *punctuation[] = {
+    ".", ",", ";", ":", "::", "\\", "\\\\", "//", "#", "(", ")", "{", "}",
 };
 
 #define NUM_KEYWORDS (sizeof(keywords) / sizeof(keywords[0]))
@@ -47,42 +51,71 @@ Token classifyKeyword(const char *str) {
     if (strcmp(str, "int") == 0) return Int;
     if (strcmp(str, "float") == 0) return Float;
     if (strcmp(str, "bool") == 0) return Bool;
+    if (strcmp(str, "True") == 0) return True;
+    if (strcmp(str, "False") == 0) return False;
+    if (strcmp(str, "Null") == 0) return Null;
+    if (strcmp(str, "module") == 0) return Module;
+    return Huh;
 }
 
-Token classifyOperator(const char *str) {
-    if (strcmp(str, "+") == 0) return Add;
-    if (strcmp(str, "-") == 0) return Sub;
-    if (strcmp(str, "*") == 0) return Mul;
-    if (strcmp(str, "/") == 0) return Div;
-    if (strcmp(str, "=") == 0) return Eq;
-    if (strcmp(str, "==") == 0) return Eqeq;
-    if (strcmp(str, "!=") == 0) return Ne;
-    if (strcmp(str, "%") == 0) return Mod;
-    if (strcmp(str, "+=") == 0) return Inc;
-    if (strcmp(str, "-=") == 0) return Dec;
-    if (strcmp(str, "*=") == 0) return Muleq;
-    if (strcmp(str, "/=") == 0) return Diveq;
-    if (strcmp(str, "&") == 0) return And;
-    if (strcmp(str, "||") == 0) return Or;
-    if (strcmp(str, "!") == 0) return Not;
+Token classifyOperator(const char *str, int length) {
+    if (length == 3 && strncmp(str, "=/=", 3) == 0) return Ne;
+    if (length == 2) {
+        if (strncmp(str, "==", 2) == 0) return Eqeq;
+        if (strncmp(str, "<=", 2) == 0) return Le;
+        if (strncmp(str, ">=", 2) == 0) return Ge;
+        if (strncmp(str, "*=", 2) == 0) return Muleq;
+        if (strncmp(str, "+=", 2) == 0) return Inc;
+        if (strncmp(str, "-=", 2) == 0) return Dec;
+        if (strncmp(str, "/=", 2) == 0) return Diveq;
+        if (strncmp(str, "||", 2) == 0) return Or;
+    }
+    if (length == 1) {
+        if (*str == '+') return Add;
+        if (*str == '-') return Sub;
+        if (*str == '*') return Mul;
+        if (*str == '/') return Div;
+        if (*str == '=') return Eq;
+        if (*str == '<') return Lt;
+        if (*str == '>') return Gt;
+        if (*str == '!') return Not;
+        if (*str == '%') return Mod;
+        if (*str == '&') return And;
+    }
+    return Huh;
+}
+
+Token classifyPunctuation(const char *str, int length) {
+    if (length == 2) {
+        if (strncmp(str, "\\\\", 2) == 0) return Dbbsl;
+        if (strncmp(str, "//", 2) == 0) return Dbfsl;
+        if (strncmp(str, "::", 2) == 0) return Dbcol;
+    }
+    if (length == 1) {
+        if (*str == ',') return Comma;
+        if (*str == '.') return Dot;
+        if (*str == ';') return Semcol;
+        if (*str == ':') return Col;
+        if (*str == '\\') return Bsl;
+        if (*str == '#') return Hash;
+        if (*str == '(') return Opb;
+        if (*str == ')') return Clb;
+        if (*str == '{') return Opclb;
+        if (*str == '}') return Clclb;
+    }
+    return Huh;
 }
 
 int isKeyword(const char *str) {
-    for(int i = 0; i < NUM_KEYWORDS; i++) {
-        if(strcmp(str, keywords[i]) == 0) {
-            return 1;
-        }
-    }
-    return 0;
+    return classifyKeyword(str) != Huh;
 }
 
 int isOperator(const char *str, int length) {
-    for (int i = 0; i < NUM_OPERATORS; i++) {
-        if (strncmp(str, operators[i], length) == 0 && strlen(operators[i]) == length) {
-            return 1;
-        }
-    }
-    return 0;
+    return classifyOperator(str, length) != Huh;
+}
+
+int isPunctuation(const char *str, int length) {
+    return classifyPunctuation(str, length) != Huh;
 }
 
 void setValue(MToken *token, const char *text) {
@@ -151,19 +184,40 @@ void next() {
             token.type = Num;
             printf("Token(NUMBER, %s)\n", token.value);
             freeToken(&token);
-        } else if (isOperator(c, 1)) {
+        } else if (isOperator(c, 3) || isOperator(c, 2) || isOperator(c, 1)) {
             const char *start = c;
-            while (*c && isOperator(c, c - start + 1)) {
-                c++;
-            }
-            int length = c - start;
+            int length = 0;
+
+            if (isOperator(c, 3)) length = 3;
+            else if (isOperator(c, 2)) length = 2;
+            else if (isOperator(c, 1)) length = 1;
+
+            c += length;
+
             char *buffer = malloc(length + 1);
             strncpy(buffer, start, length);
             buffer[length] = '\0';
+
             setValue(&token, buffer);
             free(buffer);
-            token.type = classifyOperator(token.value);
+
+            token.type = classifyOperator(start, length);
             printf("Token(OPERATOR, %s)\n", token.value);
+            freeToken(&token);
+        } else if(isPunctuation(c, 2) || isPunctuation(c, 1)) {
+            const char *start = c;
+            int length = isPunctuation(c, 2) ? 2 : 1;
+            c += length;
+
+            char *buffer = malloc(length + 1);
+            strncpy(buffer, start, length);
+            buffer[length] = '\0';
+
+            setValue(&token, buffer);
+            free(buffer);
+
+            token.type = classifyPunctuation(start, length);
+            printf("Token(PUNCTUATION, %s)\n", token.value);
             freeToken(&token);
         } else if(*c == '"' || *c == '\'') {
             char quote = *c++;
